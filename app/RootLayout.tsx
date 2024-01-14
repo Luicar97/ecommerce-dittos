5143a0eac4544749efce86fe98030fc5318e6bc9
Luicar97 <carlos_mendoza160@hotmail.com>
2024-01-13 23:22:02 -0500

    main

--- app/RootLayout.tsx
+++ app/RootLayout.tsx
@@ -3,6 +3,7 @@ import { MaterialTheme } from "@/components/Material";
 import Sidebar from "@/components/Sidebar";
 import Container from "@mui/material/Container";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 
 export default function RootLayout({
